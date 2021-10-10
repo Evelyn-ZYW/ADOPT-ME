7308e84d48ec4aedd98b04718ee2433e12d3b656
Evelyn Z <evelynzdragon@gmail.com>
2021-10-10 16:50:31 -0700

    introducing props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
   ])
 }
 
@@ -8,11 +10,13 @@ const App = () => {
   return React.createElement(
     'div',
     {},
-    React.createElement(
-      'h1',
-      {},
-      React.createElement('span', {}, 'adopt me')
-    )
+    [
+      React.createElement('h1',{},'Adopt me'),
+      ...[1, 2, 3, 4].map(i => React.createElement('h2', {}, i)),
+      React.createElement(Pet, {name: "Missing", animal: "Cat", breed: "mixed"}),
+      React.createElement(Pet, {name: "Erpang", animal: "Cat", breed: "British Shorthair"}),
+      React.createElement(Pet, {name: "Dog", animal: "Dog", breed: "Labrador cross"}),
+    ],
   );
 };
 
