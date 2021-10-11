bcd8ce2f9610fa1fe5cfe74ea49fa5a4daa00d02
Evelyn Z <evelynzdragon@gmail.com>
2021-10-10 17:17:46 -0700

    1. installed prettier to the project. 2. added .prettierrc file with an empty {} in it. 3. added script to run prettier

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed)
-  ])
-}
-
-const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      React.createElement('h1',{},'Adopt me'),
-      ...[1, 2, 3, 4].map(i => React.createElement('h2', {}, i)),
-      React.createElement(Pet, {name: "Missing", animal: "Cat", breed: "mixed"}),
-      React.createElement(Pet, {name: "Erpang", animal: "Cat", breed: "British Shorthair"}),
-      React.createElement(Pet, {name: "Dog", animal: "Dog", breed: "Labrador cross"}),
-    ],
-  );
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-);
+const App = () => {
+  return React.createElement("div", {}, [
+    // React.createElement("h1", {}, "Adopt me"),
+    // ...[1, 2, 3, 4].map(i => React.createElement('h2', {}, i)),
+    React.createElement(Pet, {
+      name: "Missing",
+      animal: "Cat",
+      breed: "mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Erpang",
+      animal: "Cat",
+      breed: "British Shorthair",
+    }),
+    React.createElement(Pet, {
+      name: "Dog",
+      animal: "Dog",
+      breed: "Labrador cross",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
