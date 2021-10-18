22b9d762b4463e1a82173c900e0525a04396d882
Evelyn Z <evelynzdragon@gmail.com>
2021-10-18 11:37:18 -0700

    context - how to use context in functional and class components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import React from "react";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -28,27 +29,30 @@ import Details from "./Details";
 // };
 
 const App = () => {
+  const theme = useState("pink");
   return (
-    <div>
-      {/* <Pet name="Missing" animal="Cat" breed="Mixed" />
+    <ThemeContext.Provider value={theme}>
+      <div>
+        {/* <Pet name="Missing" animal="Cat" breed="Mixed" />
       <Pet name="Erpang" animal="Cat" breed="British Shorthair" />
     <Pet name="Gougou" animal="Dog" breed="Mixed" /> */}
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchParams />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
